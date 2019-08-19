27f0e1e2ab772617881e978195e89b00d9d253dc
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-19 12:48:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert[4](element)
+end
